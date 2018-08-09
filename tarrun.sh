90ac8e4b7426042490c86d19886f85b0f43446af
xpressos <chrisdavies2000@gmail.com>
2018-08-09 18:49:59 +0100

    Update tarrun.sh

--- tarrun.sh
+++ tarrun.sh
@@ -5,6 +5,6 @@ TIMESTAMP=`date '+%d-%m-%Y - %H:%M:%S'`
 
 echo -e "\n\n===================== \n$TIMESTAMP \n=====================" >> $LOGFILE 
 
-sudo tar --warning='no-file-ignored' -cpzf /backup/tar/back`date +%d%m%y`.tar.gz --exclude-from=/usr/local/bin/tar-backup/tarexclude.txt / &> /var/log/tarbackup/tarbackup.log
+sudo tar --warning='no-file-ignored' -cpzf /backup/tar/back`date +%d%m%y`.tar.gz --exclude-from=/usr/local/bin/tar-backup/tarexclude.txt / 1>> /var/log/tarbackup/tarbackup.log
 
 echo -e "\n========================================== \n" >> $LOGFILE
